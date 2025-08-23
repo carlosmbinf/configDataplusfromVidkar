bc1328e407c5509a74a9191166344289521a6d00
Carlos Medina <carlosmbinf@gmail.com>
2025-08-23 01:11:22 -0300

    Actualizar la función de desconexión de VPN para usar el método asíncrono

--- index.js
+++ index.js
@@ -257,7 +257,7 @@ ejecutar = async () => {
 
                     //SI ESTA BLOQUEADO LA VPN LO DESCONECTA
                     if (user.vpn == false || user.desconectarVPN) {
-                        await server.call('updateUsersAll', user._id, { desconectarVPN: false })
+                        await server.call('updateAsyncUsersAll', user._id, { desconectarVPN: false })
                         console.log("FORZANDO DESCONEXION DE USUARIO: " + user.username)
                         await ejecutarScript('ip link delete ' + elementppp);
                     } else {
